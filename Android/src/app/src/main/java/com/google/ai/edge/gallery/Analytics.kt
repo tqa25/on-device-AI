d73b1da69582db0459d5424afcaef9a4661fd012
Wai Hon Law <whhone@google.com>
2025-07-11 11:13:10 -0700

    [gallery] migrate firebase analytic ktx library
    See https://firebase.google.com/docs/android/kotlin-migration
    
    PiperOrigin-RevId: 782044971

--- Android/src/app/src/main/java/com/google/ai/edge/gallery/Analytics.kt
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/Analytics.kt
@@ -17,9 +17,9 @@
 package com.google.ai.edge.gallery
 
 import android.util.Log
+import com.google.firebase.Firebase
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.analytics
 
 private var hasLoggedAnalyticsWarning = false
 
