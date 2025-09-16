3eb9ddcdb30ab13ea504cac474e10a4e700b0293
Google AI Edge Gallery <noreply@google.com>
2025-09-16 16:25:12 -0700

    Internal cleanup
    PiperOrigin-RevId: 807893513

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
     applicationId = "com.google.aiedge.gallery"
     minSdk = 31
     targetSdk = 35
-    versionCode = 12
+    versionCode = 13
     versionName = "1.0.7"
 
     // Needed for HuggingFace auth workflows.
